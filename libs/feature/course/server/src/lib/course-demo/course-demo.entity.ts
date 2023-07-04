9789602d41c68039867fe8716dc7804ac7a1bea2
clementg <derkeethus96@gmail.com>
2023-07-04 11:56:34 +0200

    changed CourseDemos to a OneToOne relationship

--- libs/feature/course/server/src/lib/course-demo/course-demo.entity.ts
+++ libs/feature/course/server/src/lib/course-demo/course-demo.entity.ts
@@ -1,10 +1,10 @@
 import { BaseEntity } from '@platon/core/server';
-import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 import { CourseEntity } from '../course.entity';
 
 @Entity('CourseDemos')
 export class CourseDemoEntity extends BaseEntity {
-  @ManyToOne((type) => CourseEntity, { onDelete: 'CASCADE' })
+  @OneToOne((type) => CourseEntity, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'course_id' })
   course!: CourseEntity;
 }
