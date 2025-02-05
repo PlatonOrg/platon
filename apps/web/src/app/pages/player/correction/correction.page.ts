442c72395ccc1651455a4920a8ea443af573feee
Nomehh <mathperriot@gmail.com>
2025-02-05 17:24:32 +0100

    refactor(front): first correction page rework

--- apps/web/src/app/pages/player/correction/correction.page.ts
+++ apps/web/src/app/pages/player/correction/correction.page.ts
@@ -5,9 +5,11 @@ import { NzSpinModule } from 'ng-zorro-antd/spin'
 import { PlayerCorrectionComponent } from '@platon/feature/player/browser'
 import { Player } from '@platon/feature/player/common'
 import { ResultService } from '@platon/feature/result/browser'
-import { ActivityCorrection } from '@platon/feature/result/common'
+import { ActivityCorrection, CourseCorrection } from '@platon/feature/result/common'
 import { UiErrorComponent } from '@platon/shared/ui'
 import { firstValueFrom } from 'rxjs'
+import { CourseService } from '@platon/feature/course/browser'
+import { Activity, FindCourse } from '@platon/feature/course/common'
 
 @Component({
   standalone: true,
@@ -21,24 +23,59 @@ export class PlayerCorrectionPage implements OnInit {
   protected player?: Player
   protected loading = true
   protected error: unknown
-  protected correction?: ActivityCorrection
+  protected correction?: CourseCorrection
 
   constructor(
     private readonly resultService: ResultService,
     private readonly activatedRoute: ActivatedRoute,
-    private readonly changeDetectorRef: ChangeDetectorRef
+    private readonly changeDetectorRef: ChangeDetectorRef,
+    private readonly courseService: CourseService
   ) {}
 
   async ngOnInit(): Promise<void> {
+    this.loading = true
+
     try {
-      const params = this.activatedRoute.snapshot.paramMap
-      const activityId = params.get('id') as string
-      this.correction = await firstValueFrom(this.resultService.findCorrection(activityId))
+      const courseId = this.getCourseId()
+      const activities = await this.getActivities(courseId)
+      this.correction = {
+        ActivityCorrections: (await this.getCorrections(activities)).filter(
+          (correction): correction is ActivityCorrection => correction !== undefined
+        ),
+      }
     } catch (error) {
-      this.error = error
+      this.handleError(error)
     } finally {
       this.loading = false
       this.changeDetectorRef.markForCheck()
     }
+  }
+
+  private getCourseId(): FindCourse {
+    const params = this.activatedRoute.snapshot.paramMap
+    return { id: params.get('id') } as FindCourse
+  }
+
+  private async getActivities(courseId: FindCourse): Promise<Activity[]> {
+    const course = await firstValueFrom(this.courseService.find(courseId))
+    const activities = await firstValueFrom(this.courseService.listActivities(course))
+    return activities.resources
+  }
+
+  private async getCorrections(activities: Activity[]): Promise<(ActivityCorrection | undefined)[]> {
+    return Promise.all(
+      activities.map(async (activity) => {
+        try {
+          return await firstValueFrom(this.resultService.findCorrection(activity.id))
+        } catch (error) {
+          return undefined
+        }
+      })
+    )
+  }
+
+  private handleError(error: unknown): void {
+    this.error = error
+    console.error('Error in ngOnInit', error)
   }
 }
--- libs/feature/player/browser/src/components/player-correction/player-correction.component.ts
+++ libs/feature/player/browser/src/components/player-correction/player-correction.component.ts
@@ -8,9 +8,6 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { NzBadgeModule } from 'ng-zorro-antd/badge'
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb'
-import { NzSegmentedModule } from 'ng-zorro-antd/segmented'
-import { NzSliderModule } from 'ng-zorro-antd/slider'
 
 import { ExercisePlayer } from '@platon/feature/player/common'
 import { ResultService } from '@platon/feature/result/browser'
@@ -18,12 +15,18 @@ import { ResultService } from '@platon/feature/result/browser'
 import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 import { DialogModule, DialogService } from '@platon/core/browser'
-import { ActivityCorrection, ExerciseCorrection } from '@platon/feature/result/common'
+import { CourseCorrection, ExerciseCorrection } from '@platon/feature/result/common'
 import { UiModalTemplateComponent } from '@platon/shared/ui'
 import { NzEmptyModule } from 'ng-zorro-antd/empty'
 import { PlayerService } from '../../api/player.service'
-import { PlayerExerciseComponent } from '../player-exercise/player-exercise.component'
 import { NzIconModule } from 'ng-zorro-antd/icon'
+import { PlayerReviewComponent } from '../player-review/player-review.component'
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { MatDividerModule } from '@angular/material/divider'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzCollapseModule } from 'ng-zorro-antd/collapse'
+import { PlayerCommentsComponent } from '../player-comments/player-comments.component'
 
 interface ExerciseGroup {
   exerciseId: string
@@ -47,17 +50,20 @@ interface ExerciseGroup {
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatDividerModule,
 
     NzIconModule,
     NzBadgeModule,
     NzEmptyModule,
-    NzSliderModule,
-    NzSegmentedModule,
-    NzBreadCrumbModule,
+    NzInputNumberModule,
+    NzInputModule,
+    NzGridModule,
+    NzCollapseModule,
 
     DialogModule,
 
-    PlayerExerciseComponent,
+    PlayerReviewComponent,
+    PlayerCommentsComponent,
     UiModalTemplateComponent,
   ],
 })
@@ -68,9 +74,10 @@ export class PlayerCorrectionComponent implements OnInit {
   private readonly changeDetectorRef = inject(ChangeDetectorRef)
 
   protected answers: ExercisePlayer[] = []
+  protected currentPlayer?: ExercisePlayer
 
   protected currentGroup?: ExerciseGroup | null = null
-  protected exerciseGroups: ExerciseGroup[] = []
+  protected exerciseGroups: Map<string, ExerciseGroup[]> = new Map()
 
   protected exercises: ExerciseCorrection[] = []
   protected currentExercise?: ExerciseCorrection | null = null
@@ -80,11 +87,53 @@ export class PlayerCorrectionComponent implements OnInit {
   protected selectedTabIndex = 0
   protected selectedExerciseIndex = 0
 
-  @Input() correction!: ActivityCorrection
+  protected correction?: CourseCorrection
+  protected comment = ''
+  protected grade = 0
+  protected activityExercisesMap: Map<string, { name: string; map: Map<string, ExerciseGroup> }> = new Map()
+
+  @Input() courseCorrection!: CourseCorrection
 
   async ngOnInit(): Promise<void> {
+    console.error('this.courseCorrection', this.courseCorrection)
     this.buildGroups()
-    this.onChooseGroup(this.exerciseGroups[0])
+    const firstGroup = this.activityExercisesMap
+      .get(this.activityExercisesMap.keys().next().value as string)
+      ?.map.values()
+      .next().value
+    console.error('firstGroup', firstGroup)
+    if (firstGroup) {
+      this.onChooseGroup(firstGroup)
+    }
+  }
+
+  private buildGroups(): void {
+    const activityExercisesMap = new Map<string, { name: string; map: Map<string, ExerciseGroup> }>()
+
+    for (const activity of this.courseCorrection.ActivityCorrections) {
+      const exercisesMap = new Map<string, ExerciseGroup>()
+      for (const exercise of activity.exercises) {
+        if (!exercisesMap.has(exercise.exerciseId)) {
+          exercisesMap.set(exercise.exerciseId, {
+            exerciseId: exercise.exerciseId,
+            exerciseName: exercise.exerciseName,
+            graded: exercise.correctedGrade != null,
+            users: [exercise],
+          })
+        } else {
+          const group = exercisesMap.get(exercise.exerciseId)
+          if (group) {
+            group.users.push(exercise)
+            group.graded = exercise.correctedGrade != null && group.graded
+          }
+        }
+      }
+      activityExercisesMap.set(activity.activityId, { name: activity.activityName, map: exercisesMap })
+    }
+
+    console.error('Activity Exercises Map:', activityExercisesMap)
+    this.activityExercisesMap = activityExercisesMap
+    this.changeDetectorRef.markForCheck()
   }
 
   protected async loadAnswers(exercise: ExerciseCorrection): Promise<void> {
@@ -99,7 +148,12 @@ export class PlayerCorrectionComponent implements OnInit {
         )
       ).exercises
     }
+
     this.changeDetectorRef.markForCheck()
+  }
+
+  protected onPanelClick(activityId: string): void {
+    console.error('activityId', activityId)
   }
 
   protected onChooseTab(index: number): void {
@@ -127,57 +181,67 @@ export class PlayerCorrectionComponent implements OnInit {
     this.changeDetectorRef.markForCheck()
   }
 
-  protected onChoosePreviousExercise(): void {
-    this.onChooseExercise(this.selectedExerciseIndex - 1).catch(console.error)
-  }
+  // protected onChoosePreviousExercise(): void {
+  //   this.onChooseExercise(this.selectedExerciseIndex - 1).catch(console.error)
+  // }
 
-  protected onChooseNextExercise(): void {
-    this.onChooseExercise(this.selectedExerciseIndex + 1).catch(console.error)
-  }
+  // protected onChooseNextExercise(): void {
+  //   this.onChooseExercise(this.selectedExerciseIndex + 1).catch(console.error)
+  // }
 
-  protected async onSaveGrade(): Promise<void> {
-    if (this.currentExercise && this.currentExercise != null) {
-      try {
-        await firstValueFrom(
-          this.resultService.upsertCorrection(this.currentExercise.exerciseSessionId, {
-            grade: this.correctedGrade as number,
-          })
-        )
-        this.currentExercise.correctedGrade = this.correctedGrade
-        this.buildGroups()
-        this.onChooseTab(this.selectedTabIndex)
-        this.dialogService.success('La note a été sauvegardée avec succès.')
-      } catch {
-        this.dialogService.error('Une erreur est survenue lors de la sauvegarde de la note.')
-      }
-    }
-  }
+  // protected async onSaveGrade(): Promise<void> {
+  //   if (this.currentExercise && this.currentExercise != null) {
+  //     try {
+  //       await firstValueFrom(
+  //         this.resultService.upsertCorrection(this.currentExercise.exerciseSessionId, {
+  //           grade: this.correctedGrade as number,
+  //         })
+  //       )
+  //       this.currentExercise.correctedGrade = this.correctedGrade
+  //       this.buildGroups()
+  //       this.onChooseTab(this.selectedTabIndex)
+  //       this.dialogService.success('La note a été sauvegardée avec succès.')
+  //     } catch {
+  //       this.dialogService.error('Une erreur est survenue lors de la sauvegarde de la note.')
+  //     }
+  //   }
+  // }
 
-  private buildGroups(): void {
-    const groupedExercises: ExerciseGroup[] = []
-    const groupedExerciseIds: Set<string> = new Set()
+  // private buildGroups(): void {
+  //   const groupedExercises: ExerciseGroup[] = []
+  //   const groupedExerciseIds: Set<string> = new Set()
+  //   const activityExercisesMap = new Map<string, ExerciseGroup[]>()
+  //   if (!this.correction) {
+  //     console.error('No correction')
+  //     return
+  //   }
 
-    for (const exercise of this.correction.exercises) {
-      if (!groupedExerciseIds.has(exercise.exerciseId)) {
-        groupedExerciseIds.add(exercise.exerciseId)
-        const newGroup: ExerciseGroup = {
-          exerciseId: exercise.exerciseId,
-          exerciseName: exercise.exerciseName,
-          users: [exercise],
-          graded: exercise.correctedGrade != null,
-        }
-        groupedExercises.push(newGroup)
-      } else {
-        const group = groupedExercises.find((g) => g.exerciseId === exercise.exerciseId)
-        if (group) {
-          group.users.push(exercise)
-          group.graded = exercise.correctedGrade != null && group.graded
-        }
-      }
-    }
-
-    this.exerciseGroups = groupedExercises
-  }
+  //   console.error('this.correction.ActivityCorrections', this.correction.ActivityCorrections)
+  //   for (const activity of this.correction.ActivityCorrections) {
+  //     for (const exercise of activity.exercises) {
+  //       if (!groupedExerciseIds.has(exercise.exerciseId)) {
+  //         groupedExerciseIds.add(exercise.exerciseId)
+  //         const newGroup: ExerciseGroup = {
+  //           exerciseId: exercise.exerciseId,
+  //           exerciseName: exercise.exerciseName,
+  //           users: [exercise],
+  //           graded: exercise.correctedGrade != null,
+  //         }
+  //         groupedExercises.push(newGroup)
+  //       } else {
+  //         const group = groupedExercises.find((g) => g.exerciseId === exercise.exerciseId)
+  //         if (group) {
+  //           group.users.push(exercise)
+  //           group.graded = exercise.correctedGrade != null && group.graded
+  //         }
+  //       }
+  //       activityExercisesMap.set(activity.activityName, groupedExercises)
+  //     }
+  //   }
+  //   console.error('groupedExercisesIds', groupedExerciseIds)
+  //   this.exerciseGroups = activityExercisesMap
+  //   console.error(this.exerciseGroups)
+  // }
 
   protected trackByExerciseId(_: number, group: ExerciseGroup): string {
     return group.exerciseId
