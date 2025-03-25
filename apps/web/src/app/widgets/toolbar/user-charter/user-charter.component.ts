725f572ed268d4cc11349dabf434916d39d94328
Tekpa <tekpamax97@gmail.com>
2025-03-25 17:42:46 +0100

    feat(user-charter): ajout du composant UserCharter avec modal et styles associés

---
+++ apps/web/src/app/widgets/toolbar/user-charter/user-charter.component.ts
@@ -0,0 +1,61 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { UserService } from '@platon/core/browser'
+import { NzModalModule } from 'ng-zorro-antd/modal'
+import { User, UserCharter } from '@platon/core/common'
+import { ChangeDetectorRef, EventEmitter, Input, Output } from '@angular/core'
+import { Router } from '@angular/router'
+import { firstValueFrom } from 'rxjs'
+import { inject } from '@angular/core'
+
+@Component({
+  selector: 'app-user-charter',
+  standalone: true,
+  imports: [CommonModule, NzModalModule],
+  templateUrl: './user-charter.component.html',
+  styleUrl: './user-charter.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [UserService],
+})
+export class UserCharterComponent {
+  @Input() user?: User
+  @Input() userCharter?: UserCharter
+  @Output() userCharterAccepted = new EventEmitter<UserCharter>()
+
+  userCharterModalVisible = false
+
+  private readonly router = inject(Router)
+  private readonly changeDetectorRef = inject(ChangeDetectorRef)
+
+  constructor(private readonly userService: UserService) {}
+
+  async showUserCharterModal(): Promise<void> {
+    if (!this.userCharter?.acceptedUserCharter) {
+      this.userCharterModalVisible = true
+      this.changeDetectorRef.markForCheck()
+    }
+  }
+
+  cancelUserCharter(): void {
+    this.userCharterModalVisible = false
+    void this.router.navigate(['/dashboard'])
+  }
+
+  async confirmUserCharter(): Promise<void> {
+    if (this.user?.id && !this.userCharter?.acceptedUserCharter) {
+      try {
+        const updatedUserCharter = await firstValueFrom(this.userService.acceptUserCharter(this.user.id))
+        if (updatedUserCharter.id) {
+          this.userCharter = { ...updatedUserCharter }
+          this.userCharterAccepted.emit(this.userCharter)
+        } else {
+          console.error('Updated user charter is missing a valid id')
+        }
+        this.userCharterModalVisible = false
+        this.changeDetectorRef.markForCheck()
+      } catch (error) {
+        console.error('An error occurred while updating the user charter acceptance:', error)
+      }
+    }
+  }
+}
