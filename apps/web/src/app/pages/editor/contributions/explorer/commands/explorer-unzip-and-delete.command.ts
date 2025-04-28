3894a1e7d628e5735f4ea4418e323b3a537d1d88
Nomehh <mathperriot@gmail.com>
2025-04-28 16:49:18 +0200

    feat(editor): add unzip and delete command #288

---
+++ apps/web/src/app/pages/editor/contributions/explorer/commands/explorer-unzip-and-delete.command.ts
@@ -0,0 +1,38 @@
+import { Injectable, inject } from '@angular/core'
+import { CommandGroups, ExplorerService, IExplorerCommand } from '@cisstech/nge-ide/explorer'
+import { FaIcon } from '@cisstech/nge/ui/icon'
+import { ResourceFileService } from '@platon/feature/resource/browser'
+import { ResourceFileImpl } from '../../file-system'
+import { firstValueFrom } from 'rxjs'
+
+export const EXPLORER_COMMAND_FILE_EXPORT = 'pl.explorer.commands.unzip-and-delete'
+
+const zipExtensions = ['.zip', '.jar', '.war', '.ear', '.rar']
+
+@Injectable()
+export class ExplorerCommandUnzipAndDelete implements IExplorerCommand {
+  private readonly explorerService = inject(ExplorerService)
+  private readonly resourceFileService = inject(ResourceFileService)
+
+  readonly id = EXPLORER_COMMAND_FILE_EXPORT
+  readonly icon = new FaIcon('fas fa-file-archive')
+  readonly group = CommandGroups.GROUP_CUT_COPY_PASTE
+  readonly label = 'Extraire ici et supprimer'
+
+  get enabled(): boolean {
+    const node = this.explorerService.focusedNode() as ResourceFileImpl
+    if (!node || node.readOnly) return false
+    return node.version === 'latest' && zipExtensions.some((ext) => node.uri.path.endsWith(ext))
+  }
+
+  async execute(): Promise<void> {
+    const node = this.explorerService.focusedNode() as ResourceFileImpl
+    await firstValueFrom(
+      this.resourceFileService.move(node.resourceFile, {
+        unzip: true,
+      })
+    )
+    await firstValueFrom(this.resourceFileService.delete(node.resourceFile))
+    this.explorerService.refresh()
+  }
+}
