3ef83c167c88eb41a741a2d914a90b36ed3ddff2
Nomehh <mathperriot@gmail.com>
2025-06-12 16:24:27 +0200

    feat(checkbox-group): add custom css props and keyboard shortcuts

--- libs/feature/webcomponent/src/lib/forms/checkbox-group/checkbox-group.component.ts
+++ libs/feature/webcomponent/src/lib/forms/checkbox-group/checkbox-group.component.ts
@@ -1,4 +1,15 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Injector, Input, Output, OnInit } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Injector,
+  Input,
+  Output,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+} from '@angular/core'
 import { WebComponent, WebComponentHooks } from '../../web-component'
 import { CheckboxGroupComponentDefinition, CheckboxGroupState, CheckboxItem } from './checkbox-group'
 
@@ -9,14 +20,28 @@ import { CheckboxGroupComponentDefinition, CheckboxGroupState, CheckboxItem } fr
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 @WebComponent(CheckboxGroupComponentDefinition)
-export class CheckboxGroupComponent implements WebComponentHooks<CheckboxGroupState>, OnInit {
+export class CheckboxGroupComponent implements WebComponentHooks<CheckboxGroupState>, OnInit, AfterViewInit {
   @Input() state!: CheckboxGroupState
   @Output() stateChange = new EventEmitter<CheckboxGroupState>()
 
+  @ViewChild('focusZone', { static: true }) focusZone!: ElementRef
+
+  inputMap: Map<string, () => void> = new Map([
+    ['Enter', () => this.onCheckBoxEnter()],
+    ['ArrowDown', () => (this.state.selectedIndex = Math.min(this.state.selectedIndex + 1, this.state.items.length))],
+    ['ArrowUp', () => (this.state.selectedIndex = Math.max(this.state.selectedIndex - 1, -1))],
+    ['Tab', () => (this.state.selectedIndex = (this.state.selectedIndex + 1) % this.state.items.length)],
+  ])
+
   constructor(readonly injector: Injector) {}
+
+  ngAfterViewInit(): void {
+    this.focusZone.nativeElement.addEventListener('keydown', this.onKeydown.bind(this))
+  }
 
   ngOnInit() {
     this.state.isFilled = false
+    this.state.selectedIndex = -1
   }
 
   onChangeState() {
@@ -38,5 +63,20 @@ export class CheckboxGroupComponent implements WebComponentHooks<CheckboxGroupSt
 
   trackBy(index: number, item: CheckboxItem) {
     return item.content || index
+  }
+
+  private onKeydown(event: KeyboardEvent) {
+    if (this.inputMap.has(event.key)) {
+      this.inputMap.get(event.key)?.()
+      event.preventDefault()
+    }
+  }
+
+  private onCheckBoxEnter() {
+    if (this.state.selectedIndex >= 0 && this.state.selectedIndex < this.state.items.length) {
+      const item = this.state.items[this.state.selectedIndex]
+      item.checked = !item.checked
+      this.stateChange.emit(this.state)
+    }
   }
 }
