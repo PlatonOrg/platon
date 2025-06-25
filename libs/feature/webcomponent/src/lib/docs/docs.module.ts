eb58a4ce0e7c3b16379a96a90bc8332930e1f9c6
Thomas Saillard <thomas.saillard@protonmail.com>
2025-06-25 16:50:21 +0200

    feat(showcase): change icons for local icons

--- libs/feature/webcomponent/src/lib/docs/docs.module.ts
+++ libs/feature/webcomponent/src/lib/docs/docs.module.ts
@@ -5,9 +5,10 @@ import { NgeMarkdownModule } from '@cisstech/nge/markdown'
 import { NgJsonEditorModule } from 'ang-jsoneditor'
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
 import { ShowcaseComponent } from './showcase/showcase.component'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, NzToolTipModule, NgeMarkdownModule, NgJsonEditorModule],
+  imports: [CommonModule, MatButtonModule, NzToolTipModule, NgeMarkdownModule, NgJsonEditorModule, MatIconModule],
   declarations: [ShowcaseComponent],
   exports: [ShowcaseComponent],
 })
