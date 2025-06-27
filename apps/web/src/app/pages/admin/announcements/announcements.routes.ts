23306493232108856c3054d379d7546b978d06d0
Tekpa <tekpamax97@gmail.com>
2025-06-27 17:35:20 +0200

    refactor(feature-announcement): supprimer les composants et les styles liés à l'édition des annonces

--- apps/web/src/app/pages/admin/announcements/announcements.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router'
-import { AdminAnnouncementsPage } from './announcements.page'
-
-export default [
-  {
-    path: '',
-    component: AdminAnnouncementsPage,
-  },
-] as Routes
--- libs/feature/announcement/browser/src/lib/components/index.ts
+++ libs/feature/announcement/browser/src/lib/components/index.ts
@@ -1,4 +1,3 @@
 export * from './feature-announcement-modal/feature-announcement-modal.component';
 export * from './announcement-create-drawer/announcement-create-drawer.component';
-export * from './announcement-edit-drawer/announcement-edit-drawer.component';
 //export * from './announcement-list/announcement-list.component';
