2306da08a2a45989b196f4574dfbe13f2addf2be
Thomas Saillard <thomas.saillard@protonmail.com>
2025-07-01 10:36:51 +0200

    feat(editorjs): Editor.js viewer service to HTML

--- libs/feature/webcomponent/src/lib/internals/editorjs-viewer/editorjs-viewer.module.ts
+++ libs/feature/webcomponent/src/lib/internals/editorjs-viewer/editorjs-viewer.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common'
 import { NgModule, Type } from '@angular/core'
 import { IDynamicModule } from '@cisstech/nge/services'
 import { EditorjsViewerComponent } from './editorjs-viewer.component'
+import { EditorjsViewerService } from '@platon/shared/utils'
 
 @NgModule({
   imports: [CommonModule],
   declarations: [EditorjsViewerComponent],
   exports: [EditorjsViewerComponent],
+  providers: [EditorjsViewerService],
 })
 export class EditorjsViewerModule implements IDynamicModule {
   component: Type<unknown> = EditorjsViewerComponent
