0d1af541ef8e78a93058d77ac565241fcc9829c0
Nomehh <mathperriot@gmail.com>
2025-07-09 10:58:55 +0200

    fix(webcomponent): remove useless imports

--- libs/feature/webcomponent/src/lib/widgets/confetti/confetti.module.ts
+++ libs/feature/webcomponent/src/lib/widgets/confetti/confetti.module.ts
@@ -1,13 +1,11 @@
 import { NgModule, Type } from '@angular/core'
-import { NgeMonacoModule } from '@cisstech/nge/monaco'
 import { BaseModule } from '../../shared/components/base/base.module'
 import { IDynamicModule } from '@cisstech/nge/services'
-import { NgxEchartsModule } from 'ngx-echarts'
 import { ConfettiComponent } from './confetti.component'
 
 @NgModule({
   declarations: [ConfettiComponent],
-  imports: [BaseModule, NgeMonacoModule, NgxEchartsModule],
+  imports: [BaseModule],
   exports: [ConfettiComponent],
 })
 export class ConfettiModule implements IDynamicModule {
