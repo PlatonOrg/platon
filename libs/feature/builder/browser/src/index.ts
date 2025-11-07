e1e8b9ada92cb7ef3a2ebb9b86ca4f44468ac3bc
Tekpa Max <tekpamax97@gmail.com>
2025-11-07 10:35:54 +0100

    refactor(builder-settings): remove unused settings component and related files

--- libs/feature/builder/browser/src/index.ts
+++ libs/feature/builder/browser/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/api/builder.service'
 export * from './lib/component/ai-prompt-modal/ai-prompt-modal.component'
 export * from './lib/component/builder-iframe/builder-iframe.component'
-export * from './lib/component/settings/settings.component'
 export * from './providers'
