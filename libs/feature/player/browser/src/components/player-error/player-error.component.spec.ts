5310b48dbcfee1a4f56c55804d1e81b4c3629bf0
Max Tekpa <tekpamax97@gmail.com>
2026-07-07 10:39:59 +0200

    test(player-error): add mock player input for component tests test(correction): implement unit tests for CorrectionService test(aggregators): enhance answer state checks for error handling fix(tsconfig): ensure target is set to ES2018 for spec files

--- libs/feature/player/browser/src/components/player-error/player-error.component.spec.ts
+++ libs/feature/player/browser/src/components/player-error/player-error.component.spec.ts
@@ -1,9 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ExercisePlayer } from '@platon/feature/player/common'
 import { PlayerErrorComponent } from './player-error.component'
 
 describe('PlayerErrorComponent', () => {
   let component: PlayerErrorComponent
   let fixture: ComponentFixture<PlayerErrorComponent>
+
+  const mockPlayer: ExercisePlayer = {
+    type: 'exercise',
+    sessionId: 'session-test-id',
+    form: '',
+    title: 'Exercice test',
+    statement: '',
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,6 +21,7 @@ describe('PlayerErrorComponent', () => {
 
     fixture = TestBed.createComponent(PlayerErrorComponent)
     component = fixture.componentInstance
+    fixture.componentRef.setInput('player', mockPlayer)
     fixture.detectChanges()
   })
 
