fb9152434e863069d3a9ca13f4f2228caa313338
Max Tekpa <tekpamax97@gmail.com>
2026-09-01 17:22:08 +0200

    feat(player-wrapper): update player input handling and enhance embedded styles for better integration

--- libs/feature/player/browser/src/components/player-wrapper/player-wrapper.component.ts
+++ libs/feature/player/browser/src/components/player-wrapper/player-wrapper.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, input } from '@angular/core'
 
 import { Player } from '@platon/feature/player/common'
 import { UiErrorComponent } from '@platon/shared/ui'
@@ -12,8 +12,10 @@ import { PlayerExerciseComponent } from '../player-exercise/player-exercise.comp
   templateUrl: './player-wrapper.component.html',
   styleUrls: ['./player-wrapper.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { '[class.embedded]': 'embedded()' },
   imports: [CommonModule, UiErrorComponent, PlayerExerciseComponent, PlayerActivityComponent],
 })
 export class PlayerWrapperComponent {
-  @Input({ required: true }) player!: Player
+  readonly player = input.required<Player>()
+  readonly embedded = input(false)
 }
